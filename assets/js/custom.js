d1cf89200ed64a67302545edcde1c22190b1c35f
Responsibleuk <james@we-are-responsible.com>
2023-08-12 15:09:36 +0000

    Add

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -297,15 +297,15 @@ var swiper = new Swiper('.herotwoswiper', {
     });
 }
 /* 12. searchBtn */
-const input = document.getElementById("search-input");
-const searchBtn = document.getElementById("search-btn");
+// const input = document.getElementById("search-input");
+// const searchBtn = document.getElementById("search-btn");
 
-const expand = () => {
-  searchBtn.classList.toggle("close");
-  input.classList.toggle("square");
-};
+// const expand = () => {
+//   searchBtn.classList.toggle("close");
+//   input.classList.toggle("square");
+// };
 
-searchBtn.addEventListener("click", expand);
+// searchBtn.addEventListener("click", expand);
 
 
 /* 13. mobile-nav */ 
